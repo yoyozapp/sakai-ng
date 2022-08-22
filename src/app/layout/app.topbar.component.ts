e514f0898a68312bd90c8331fe3ce41333c9164a
BPongz <89505707+BPongz@users.noreply.github.com>
2022-08-22 14:01:29 +0700

    navbar update

--- src/app/layout/app.topbar.component.ts
+++ src/app/layout/app.topbar.component.ts
@@ -1,14 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MenuItem } from 'primeng/api';
-import { LayoutService } from "./service/app.layout.service";
+import { LayoutService } from './service/app.layout.service';
+import { timer } from 'rxjs';
 
 @Component({
     selector: 'app-topbar',
-    templateUrl: './app.topbar.component.html'
+    templateUrl: './app.topbar.component.html',
 })
 export class AppTopBarComponent {
-
+    time?: Date;
     items!: MenuItem[];
 
-    constructor(public layoutService: LayoutService) { }
+    constructor(public layoutService: LayoutService) {}
+    ngOnInit(): void {
+        timer(0, 1000).subscribe(() => {
+            this.time = new Date();
+        });
+    }
 }
